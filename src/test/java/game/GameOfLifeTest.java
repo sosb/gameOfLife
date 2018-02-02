c8b72b75444c8a81a971c5bacbb0932d8787a0c3
sos.bence <sos.bence@virgo.hu>
2018-02-02 17:12:32 +0100

    game

---
+++ src/test/java/game/GameOfLifeTest.java
@@ -0,0 +1,38 @@
+package game;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameOfLifeTest {
+
+    @Test
+    public void testDead() {
+        boolean[][] area = {{false}};
+        Assert.assertArrayEquals(area, GameOfLife.tick(area));
+    }
+
+    @Test
+    public void testAlive() {
+        boolean[][] area = {{true}};
+        Assert.assertArrayEquals(new boolean[][]{{false}}, GameOfLife.tick(area));
+    }
+
+    @Test
+    public void testTwoLong() {
+        boolean[][] area = {{true, false}};
+        Assert.assertArrayEquals(new boolean[][]{{false, false}}, GameOfLife.tick(area));
+    }
+
+    @Test
+    public void testThreeLong() {
+        boolean[][] area = {{true, true, true}};
+        Assert.assertArrayEquals(new boolean[][]{{false, true, false}}, GameOfLife.tick(area));
+    }
+
+    @Test
+    public void test2d() {
+        boolean[][] area = {{false, true, false}, {false, true, false}, {false, true, false}};
+        final boolean[][] expected = {{false, false, false}, {false, true, false}, {false, false, false}};
+        Assert.assertArrayEquals(expected, GameOfLife.tick(area));
+    }
+}
